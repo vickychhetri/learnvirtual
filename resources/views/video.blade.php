58a329e619b180b651c3fce26b5ab13d750735a1
vicky chhetri <specialpower321@gmail.com>
2022-01-10 20:45:06 +0530

    video

---
+++ resources/views/video.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Dash.js Rocks</title>
+        <style>
+            video {
+                width: 640px;
+                height: 360px;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+            <video data-dashjs-player autoplay src="/Videos/module1/manifest.mpd" controls>
+            </video>
+        </div>
+        <script src="/dashjs/dash.all.min.js"></script>
+    </body>
+</html>
