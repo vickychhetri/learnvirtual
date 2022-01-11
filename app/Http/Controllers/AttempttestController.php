fa69c4979ce9d816e94d8a38ec4bd20ef65bf763
vicky chhetri <specialpower321@gmail.com>
2022-01-11 15:28:20 +0530

    database

---
+++ app/Http/Controllers/AttempttestController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\attempttest;
+use Illuminate\Http\Request;
+
+class AttempttestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\attempttest  $attempttest
+     * @return \Illuminate\Http\Response
+     */
+    public function show(attempttest $attempttest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\attempttest  $attempttest
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(attempttest $attempttest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\attempttest  $attempttest
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, attempttest $attempttest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\attempttest  $attempttest
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(attempttest $attempttest)
+    {
+        //
+    }
+}
